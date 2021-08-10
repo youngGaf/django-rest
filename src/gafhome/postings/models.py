ea6a5b236f90defbc27dc39866e2a6567d61e64d
youngGaf <youngabuhafiz@gmail.com>
2021-08-10 20:15:38 +0200

    model setup for BlogPost

--- src/gafhome/postings/models.py
+++ src/gafhome/postings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+
+class BlogPost(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120, null=True, blank=True)
+    content = models.TextField(max_length=120, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user
